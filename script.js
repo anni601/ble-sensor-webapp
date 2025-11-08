2219a5f3717cc98cb61b704f0758598cb1e8266a
anni601 <annika.becherer@hs-augsburg.de>
2025-11-08 14:36:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const serviceUUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
-const characteristicUUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+const serviceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+const characteristicUUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 let characteristic;
 
 document.getElementById("connectBtn").addEventListener("click", async () => {
